a97d0ad90dcb380bffac4e4d266cfa9ffa8974cc
rgadellaa <roderick.gadellaa@brightside.nl>
2022-11-25 10:15:07 +0100

    Prepare demo

--- src/js/papers/print.js
+++ src/js/papers/print.js
@@ -11,4 +11,27 @@
     details.forEach( element => element.removeAttribute('open') );
   });
 
+  // @TODO: Move this into eleventy plugin so we perform
+  // this at build time, not on the client
+  const content = document.querySelector('.content')
+  const { children } = content;
+  let section;
+  [ ...children ].forEach( child => {
+
+    if (child.tagName === 'H2') {
+      if (section)
+        content.append( section );
+      section = document.createElement('section');
+      section.classList.add('flow');
+    }
+
+    section.append( child );
+
+  });
+
+  if (section)
+    content.append( section );
+
+  content.classList.remove('flow');
+
 })();

8b6c5fe69391e39b9a16aa8b0f60400057445d39
Roderick Gadellaa <ik@rejh.nl>
2024-03-05 19:11:39 +0100

    Target .dma-countdown elements, tweak some settings, only run on intereset...

--- src/js/dma-confetti.js
+++ src/js/dma-confetti.js
@@ -11,21 +11,36 @@ const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
+const dlTime = Date.parse( DEADLINE );
+const nowTime = Date.now();
+
+const weakMap = new WeakMap();
+
 function _atRandomInterval( fn, n = 0 ) {
   if ( n >= TIMES ) return;
   const fnWrap = () => { fn(); _atRandomInterval( fn, n+1 ); }
   setTimeout( fnWrap, MIN_TIME + Math.random() * MAX_TIME );
 }
 
-_atRandomInterval( confetti );
-confetti();
+const intersectionObs = new IntersectionObserver( entries => {
+  entries.forEach( entry => {
+    if ( !entry.isIntersecting ) return;
+    const confettiFn = weakMap.get( entry.target );
+    confettiFn.reset();
+    _atRandomInterval(() => {
+      confettiFn({
+        colors: [ '#000', ]
+      })
+    });
+  })
+});
 
 [ ...document.querySelectorAll('.dma-countdown') ].forEach(
   parent => {
-
-    const dlTime = Date.parse( DEADLINE );
-    const nowTime = Date.now();
-    // confetti();
-
+    weakMap.set(
+      parent,
+      confetti.create( parent.querySelector('canvas'), { resize: true } )
+    );
+    intersectionObs.observe( parent );
   }
 );

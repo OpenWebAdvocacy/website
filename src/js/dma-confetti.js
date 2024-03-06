d78750482a8f8ad2c45a33c6d96e99468b10353f
Roderick Gadellaa <ik@rejh.nl>
2024-03-06 07:16:19 +0100

    Improve; - Add confetti to all pages - Use interval to fire confetti as soon as countdown hits zero - Tweak confetti options - Clean up code

--- src/js/dma-confetti.js
+++ src/js/dma-confetti.js
@@ -2,45 +2,66 @@
 import confetti from 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.module.min.mjs';
 
 const DEADLINE = '2024-03-06T23:00:00Z'; // 23:00:00 UTC = 00:00:00 CET
-const TIMES = 10;
+
+const TIMES = 5;
 const MIN_TIME = 500;
 const MAX_TIME = 2000;
+
+const CONFETTI_GLOBAL_OPTS = {
+  colors: ['#000'],
+  scalar: 1.5,
+};
+const CONFETTI_OPTS = {
+  resize: true,
+  disableForReducedMotion: true,
+}
 
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
 const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
-const dlTime = Date.parse( DEADLINE );
-const nowTime = Date.now();
+function _init() {
 
-const weakMap = new WeakMap();
+  clearInterval( initInterval );
+
+  const intersectionObs = new IntersectionObserver( entries => {
+    entries.forEach( entry => {
+      if ( !entry.isIntersecting ) return;
+      const confettiFn = weakMap.get( entry.target );
+      confettiFn.reset();
+      _atRandomInterval(() => {
+        confettiFn( CONFETTI_GLOBAL_OPTS )
+      });
+      confettiFn( CONFETTI_GLOBAL_OPTS )
+    })
+  });
+
+  [ ...document.querySelectorAll('.dma-countdown') ].forEach(
+    parent => {
+      weakMap.set(
+        parent,
+        confetti.create( parent.querySelector('canvas'), CONFETTI_OPTS )
+      );
+      intersectionObs.observe( parent );
+    }
+  );
+
+}
 
 function _atRandomInterval( fn, n = 0 ) {
   if ( n >= TIMES ) return;
   const fnWrap = () => { fn(); _atRandomInterval( fn, n+1 ); }
-  setTimeout( fnWrap, MIN_TIME + Math.random() * MAX_TIME );
+  setTimeout( fnWrap, MIN_TIME + ( Math.random() * MAX_TIME ) );
 }
 
-const intersectionObs = new IntersectionObserver( entries => {
-  entries.forEach( entry => {
-    if ( !entry.isIntersecting ) return;
-    const confettiFn = weakMap.get( entry.target );
-    confettiFn.reset();
-    _atRandomInterval(() => {
-      confettiFn({
-        colors: [ '#000', ]
-      })
-    });
-  })
-});
+function _now() {
+  return Date.now();
+}
 
-[ ...document.querySelectorAll('.dma-countdown') ].forEach(
-  parent => {
-    weakMap.set(
-      parent,
-      confetti.create( parent.querySelector('canvas'), { resize: true } )
-    );
-    intersectionObs.observe( parent );
-  }
-);
+const dlTime = Date.parse( DEADLINE );
+const weakMap = new WeakMap();
+
+const initInterval = setInterval(() => {
+  if ( dlTime <= _now() ) _init();
+}, 1000);
